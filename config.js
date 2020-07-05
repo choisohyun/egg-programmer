7bae2c02156bf9e7602b3646bd49fab13c6ffdbe
choisohyun <sohyun4283@naver.com>
2020-07-05 22:44:47 +0900

    docs. tags 이름, 설명 정리

--- config.js
+++ config.js
@@ -19,64 +19,48 @@ module.exports = {
     github: 'https://github.com/choisohyun',
     rss: '/rss.xml',
   },
-  contactFormUrl:
-    process.env.CONTACT_FORM_ENDPOINT
-    || 'https://getform.io/f/09a3066f-c638-40db-ad59-05e4ed71e451',
   googleAnalyticTrackingId: process.env.GA_TRACKING_ID || '',
   tags: {
     javascript: {
       name: 'javascript',
       description:
-        'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.',
+        'JavaScript는 웹 페이지에 기능을 제공하기 위해 HTML 및 CSS와 함께 사용되는 프로그래밍 언어입니다.',
       color: '#f0da50',
     },
     nodejs: {
       name: 'Node.js',
       description:
-        'Node.js is a tool for executing JavaScript in a variety of environments.',
+        'Node.js는 다양한 환경에서 JavaScript를 실행하기위한 도구입니다.',
       color: '#90c53f',
     },
-    rxjs: {
-      name: 'RxJS',
-      description:
-        'RxJS is a library for reactive programming using Observables, for asynchronous operations.',
-      color: '#eb428e',
+    undefined: {
+      name: 'undefined',
+      description: '이전 블로그 포스팅에서 태그가 정해져 있지 않은 경우입니다.',
+      color: '#00bbff',
     },
     typescript: {
       name: 'typescript',
       description:
-        'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
+        'TypeScript는 일반 JavaScript에 타입을 지정해 주기 위해 사용하는 라이브러리입니다.',
       color: '#257acc',
     },
     reactjs: {
       name: 'reactjs',
       description:
-        'React is an open source JavaScript library used for designing user interfaces.',
+        'React는 UI 디자인에 사용되는 오픈 소스 JavaScript 라이브러리입니다.',
       color: '#61dbfa',
-    },
-    gatsby: {
-      name: 'Gatsby.js',
-      description:
-        'A framework built over ReactJS to generate static page web application.  ',
-      color: '#6f309f',
     },
     html: {
       name: 'HTML',
       description:
-        'A markup language that powers the web. All websites use HTML for structuring the content.',
+        '웹을 강화하는 마크 업 언어입니다. 모든 웹 사이트는 HTML을 사용하여 컨텐츠를 구성합니다.',
       color: '#dd3431',
     },
     css: {
       name: 'css',
       description:
-        'CSS is used to style the HTML element and to give a very fancy look for the web application.',
+        'CSS는 HTML 요소의 스타일을 지정하고 웹 응용 프로그램을 매우 멋지게 보여줍니다.',
       color: '#43ace0',
-    },
-    python: {
-      name: 'python',
-      description:
-        'A general purpose programming language that is widely used for developing various applications.',
-      color: '#f9c646',
     },
   },
 };
